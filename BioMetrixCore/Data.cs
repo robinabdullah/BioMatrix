95d0f36fb00bbd4910c8556292c8a61e59109f47
robinabdullah <robinabdullah@yahoo.com>
2018-05-06 22:20:31 +0600

    Add project files.

---
+++ BioMetrixCore/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioMetrixCore
+{
+    class Data
+    {
+        public int Id { set; get; }
+        public string EntryTime { set; get; }
+    }
+}
